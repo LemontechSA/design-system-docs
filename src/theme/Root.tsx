d3af909a0c390a6c9b14af64917340d8dc0f8799
José Núñez <jose.nunez.rv@gmail.com>
2022-10-03 12:33:35 -0300

    cleanning

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-import { ThemeProvider } from "lemon-system";
-
-/* export default function Root({ children }) {
-  return <ThemeProvider>{children}</ThemeProvider>;
-} */
-
 export default function Root({ children }) {
   return children;
 }
