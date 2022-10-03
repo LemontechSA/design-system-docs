8e4551198d7b53a599766e615f26ef59e35536cf
José Núñez <jose.nunez.rv@gmail.com>
2022-10-03 11:58:54 -0300

    feature: gh pages

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { ThemeProvider } from "lemon-system";
 
+/* export default function Root({ children }) {
+  return <ThemeProvider>{children}</ThemeProvider>;
+} */
+
 export default function Root({ children }) {
-  return <ThemeProvider resetCSS={false}>{children}</ThemeProvider>;
+  return children;
 }
