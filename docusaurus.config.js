52a2dd8269fff9e266265640a2636da340e737d3
José Núñez <jose.nunez.rv@gmail.com>
2022-10-24 12:48:02 -0300

    fix: change baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   title: "Lemon System",
   tagline: "Design System for lawyers",
   url: "https://lemontechsa.github.io",
-  baseUrl: "/design-system-docs/",
+  baseUrl: "/suit-ui-docs/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "LemontechSA", // Usually your GitHub org/user name.
-  projectName: "design-system-docs", // Usually your repo name.
+  projectName: "suit-ui-docs", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
